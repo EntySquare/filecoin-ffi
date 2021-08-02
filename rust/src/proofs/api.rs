aa7922cf0e2fe0f9107aadbe20bafdba72f0385a
guodayang999 <33561649+guodayang999@users.noreply.github.com>
2021-08-02 11:21:26 +0800

    log scp1o data

--- rust/src/proofs/api.rs
+++ rust/src/proofs/api.rs
@@ -372,14 +372,22 @@ pub unsafe extern "C" fn fil_seal_commit_phase2(
         init_log();
 
         info!("seal_commit_phase2: start");
-
         let mut response = fil_SealCommitPhase2Response::default();
+
+        println!("===============================================");
+        println!("seal_commit_phase1_output_ptr:{}",seal_commit_phase1_output_ptr);
+        println!("seal_commit_phase1_output_len:{}",seal_commit_phase1_output_len);
+        println!("===============================================");
 
         let scp1o = serde_json::from_slice(from_raw_parts(
             seal_commit_phase1_output_ptr,
             seal_commit_phase1_output_len,
         ))
         .map_err(Into::into);
+
+        println!("scp1o:{:#?}",scp1o);
+        println!("===============================================");
+
 
         let result =
             scp1o.and_then(|o| seal_commit_phase2(o, prover_id.inner, SectorId::from(sector_id)));
